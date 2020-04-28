f9ab07927c1fe6dc4cf18ad2a24808f941836ae8
liam <liamamadio@gmail.com>
2020-04-28 20:03:46 +0100

    added email function

--- miata_finder.py
+++ miata_finder.py
@@ -6,18 +6,29 @@ from cache import cache_list
 import os
 
 
+
 class Miata:
-    def __init__(self, name, pid, price, city):
+    def __init__(self, name, pid, price, city, url="no-url"):
         self.name = name
         self.pid = pid
         self.price = price
         self.city = city
+        self.url = url
 
+def filterDuplicates(miata_list):
+    '''
+    filters out duplicates comming from different cities
+    '''
+    for miatas in miata_list:
+        for in_miatas in miata_list:
+            if (miatas.pid == in_miatas.pid and miatas.city != in_miatas.city):
+                miata_list.pop(miata_list.index(in_miatas))
+    return miata_list
 
 
 def craigslistSearch():
     '''
-    parses craigslist miata serach and returns miata object list
+    parses craigslist miata serach and returns miata object list; calls filterDuplicates()
     '''
     cities = ["houston","austin","dallas","waco"]
     miata_list = [] #holds miata objects
@@ -29,13 +40,35 @@ def craigslistSearch():
         miata_prices = soup.find_all("span", {"class": "result-price"})
         miata_namesandpid = soup.find_all("a",{"class": "result-title hdrlnk"})
         for price, npid in zip(miata_prices[::2], miata_namesandpid):
-            if "miata" not in npid.text.lower():
+            if "miata" not in npid.text.lower(): #filters out results that dont have the word miata in them
                 continue
             elif npid in miata_list:
                 continue
             else:
                 miata_list.append(Miata(npid.text, npid.get("data-id"), price.text,city))
+
+
+    miata_list = filterDuplicates(miata_list)
+    
     return miata_list
+
+
+
+def emailNewMiatas(not_in):
+    '''
+    takes in miata object list from c_checkCache() and extends each miata in the list to one strong so that it can be emailed
+    '''
+    emailList = []
+    for miatas in not_in:
+        emailList.append("{}\nPrice: \{}\nLocation:{}\nLink: {}\n-----\n".format(miatas.name, miatas.price, miatas.city, miatas.url))
+
+
+    newstring = "".join(emailList)
+    if newstring != "":
+        emailString = 'echo "{}" | mail -s "New Miata Listings!" liamamadio@gmail.com'.format(newstring)
+        os.system(emailString)
+        print(newstring)
+
 
 
 def c_updateCache(miata_list):
@@ -50,7 +83,7 @@ def c_updateCache(miata_list):
 
 def c_checkCache():
     '''
-    parses craigslist miata search and compares it to cache to find changes returns a list of miata objects
+    parses craigslist miata search and compares it to cache to find changes returns a list of miata objects that werent previously in the cache
     '''
     not_in = []
     current_pids = craigslistSearch()
@@ -58,14 +91,19 @@ def c_checkCache():
         if str(pids.pid) not in str(cache_list):
             not_in.append(pids)
             emailString = "{}\nPrice:\{}\nLocation:{}".format(pids.name,pids.price,pids.city)
-            os.system('echo "{}" | mail -s "New Miata Listing!" liamamadio@gmail.com'.format(emailString))
+            #os.system('echo "{}" | mail -s "New Miata Listing!" liamamadio@gmail.com'.format(emailString))
             print(pids.name,":",pids.price)
     return not_in
 
 #testing
 
 def main():
+
+    miata_list = craigslistSearch()
     not_in = c_checkCache()
+
+    emailNewMiatas(not_in)
+
     c_updateCache(craigslistSearch())
 main()
 
